6df5f0acc579fb3c734ff2754bd09ce29f0deb3b
vanessa <vanessaduquerubio@gmail.com>
2023-08-22 13:03:58 +0200

    creacion de todas la paginas y comprovado que funcionan las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { HerramientasComponent } from './pages/herramientas/herramientas.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
+    HerramientasComponent,
+    ProyectosComponent,
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
