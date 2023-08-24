21952481d1c3f5eae652c296567d466ee262e5f5
vanessa <vanessaduquerubio@gmail.com>
2023-08-24 06:35:24 +0200

    cambios provisionales

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  @ViewChild('maquina', { static: true }) maquina!: ElementRef;
 
+  private instanciaTyped: Typed | null = null;
+
+  ngOnInit() {
+    this.instanciaTyped = new Typed(this.maquina.nativeElement, {
+      strings: ['Vanessa Duque Rubio', 'FullStack Developer.'],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1500
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.instanciaTyped) {
+      this.instanciaTyped.destroy();
+    }
+  }
 }
