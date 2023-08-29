688dcb88a6c7caca9ccedd55d5f08f4f58850e77
vanessa <vanessaduquerubio@gmail.com>
2023-08-29 16:24:00 +0200

    casi casi terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
-import { HerramientasComponent } from './pages/herramientas/herramientas.component';
-import { ProyectosComponent } from './pages/proyectos/proyectos.component';
-import { ContactoComponent } from './pages/contacto/contacto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HerramientasComponent } from './components/herramientas/herramientas.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CarouselModule, } from 'ngx-owl-carousel-o';
 import { FooterComponent } from './components/footer/footer.component';
